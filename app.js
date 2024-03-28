0ffce14e4b2382689d5d3fe49b83cd2674d77536
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-03-28 12:58:11 +0400

    JS-02

---
+++ app.js
@@ -0,0 +1,76 @@
+//Task-01;
+// let twoDigitNum = +prompt("Ikireqemli Eded Daxil Edin ");
+// while (twoDigitNum < 100) {
+//   twoDigitNum = twoDigitNum + 7;
+// }
+// console.log(twoDigitNum);
+//Task-02;
+// let nNumber = +prompt(" N Eded-i Daxil Edin");
+// let i = 0;
+// while (nNumber > i) {
+//   console.log("I know how to use cycles");
+//   i++;
+// };
+//Task-03;
+// let i = 100;
+// while (i <= 999) {
+//   if (i % 10 === 0) {
+//     console.log(i);
+//   }
+//     i++;
+// }
+//Task-04;
+// let i = 10;
+// let sum = 0;
+// while (i <= 99) {
+//   if (i % 2 === 1) {
+//     console.log(i);
+//     sum += i;
+//   }
+//   i++;
+// }
+// console.log(sum);
+//Task-05;
+// let userInput = +prompt("Enter a number:");
+// let sum = 0;
+// for (let i = 100; i <= 999; i++) {
+//   if (userInput % i === 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+//Task-06;
+// let sum = 0;
+// let getNumber = +prompt("N ededi daxil Edin...");
+// while (getNumber > 0) {
+//   console.log(getNumber);
+//   sum -= getNumber;
+//   getNumber--;
+// }
+//Task-07;
+// let num = 10;
+// let sum = 1;
+// for (let i = 1; i <= num; i++) {
+//   console.log("hello");
+//   sum *= i;
+// }
+// console.log(sum);
+//Task-08;
+// let userInput = prompt("Enter some text:");
+// let sortElement = "";
+// for (let i = userInput.length-1; i >= 0; i--) {
+//   sortElement += userInput[i];
+// }
+// console.log(sortElement);
+//Task -9;
+// var text =
+//   '"Mətndə "." hərfinin ilk dəfə hansi indeksde yerləşdiyini tapan proqram tertib edin."';
+// var index = 0;
+// for (var i = 0; i < text.length; i++) {
+//   if (text[i] === ".") {
+//     index = i;
+//     break;
+//   }
+// }
+// console.log(index);
+//Task -10;
